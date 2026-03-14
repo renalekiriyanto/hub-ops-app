1fa8e50a71faf812c6ac83d72f2b70fef2e69679
Renal Eki Riyanto <ekysr99@gmail.com>
2026-03-14 15:58:13 +0700

    Change theme

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,21 +1,115 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
+<!--begin::Head-->
 @include('layouts.header')
+<!--end::Head-->
+<!--begin::Body-->
 
-<body>
-    <div class="container-scroller">
-        @include('layouts.navbar')
-        <!-- partial -->
-        <div class="container-fluid page-body-wrapper">
-            @include('layouts.sidebar')
-            @yield('content')
-            <!-- main-panel ends -->
-        </div>
-        <!-- page-body-wrapper ends -->
+<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+        <!--begin::Header-->
+        <nav class="app-header navbar navbar-expand bg-body">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::End Navbar Links-->
+                <ul class="navbar-nav ms-auto">
+                    <!--begin::User Menu Dropdown-->
+                    <li class="nav-item dropdown user-menu">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <img src="./assets/img/user2-160x160.jpg" class="user-image rounded-circle shadow"
+                                alt="User Image" />
+                            <span class="d-none d-md-inline">Alexander Pierce</span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
+                            <!--begin::User Image-->
+                            <li class="user-header text-bg-primary">
+                                <img src="./assets/img/user2-160x160.jpg" class="rounded-circle shadow"
+                                    alt="User Image" />
+                                <p>
+                                    Alexander Pierce - Web Developer
+                                    <small>Member since Nov. 2023</small>
+                                </p>
+                            </li>
+                            <!--end::User Image-->
+                            <!--begin::Menu Body-->
+                            <li class="user-body">
+                                <!--begin::Row-->
+                                <div class="row">
+                                    <div class="col-4 text-center">
+                                        <a href="#">Followers</a>
+                                    </div>
+                                    <div class="col-4 text-center">
+                                        <a href="#">Sales</a>
+                                    </div>
+                                    <div class="col-4 text-center">
+                                        <a href="#">Friends</a>
+                                    </div>
+                                </div>
+                                <!--end::Row-->
+                            </li>
+                            <!--end::Menu Body-->
+                            <!--begin::Menu Footer-->
+                            <li class="user-footer">
+                                <a href="#" class="btn btn-outline-secondary">Profile</a>
+                                <a href="#" class="btn btn-outline-danger float-end">Sign out</a>
+                            </li>
+                            <!--end::Menu Footer-->
+                        </ul>
+                    </li>
+                    <!--end::User Menu Dropdown-->
+                </ul>
+                <!--end::End Navbar Links-->
+            </div>
+            <!--end::Container-->
+        </nav>
+        <!--end::Header-->
+        <!--begin::Sidebar-->
+        @include('layouts.sidebar')
+        <!--end::Sidebar-->
+        <!--begin::App Main-->
+        <main class="app-main">
+            <!--begin::App Content Header-->
+            <div class="app-content-header">
+                <!--begin::Container-->
+                <div class="container-fluid">
+                    <!--begin::Row-->
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h3 class="mb-0">Dashboard</h3>
+                        </div>
+                    </div>
+                    <!--end::Row-->
+                </div>
+                <!--end::Container-->
+            </div>
+            <!--end::App Content Header-->
+            <!--begin::App Content-->
+            <div class="app-content">
+                <!--begin::Container-->
+                <div class="container-fluid">
+                    <!--begin::Row-->
+                    <!--end::Row-->
+                    <!--begin::Row-->
+                    <div class="row">
+
+                    </div>
+                    <!-- /.row (main row) -->
+                </div>
+                <!--end::Container-->
+            </div>
+            <!--end::App Content-->
+        </main>
+        <!--end::App Main-->
+        <!--begin::Footer-->
+        @include('layouts.footer')
+        <!--end::Footer-->
     </div>
+    <!--end::App Wrapper-->
+    <!--begin::Script-->
     @include('layouts.scripts')
-    <!-- End custom js for this page -->
+    <!--end::Script-->
 </body>
+<!--end::Body-->
 
 </html>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,51 +1,205 @@
-<!-- partial:partials/_sidebar.html -->
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-        <li class="nav-item nav-profile">
-            <a href="#" class="nav-link">
-                <div class="nav-profile-image">
-                    <img src="assets/images/faces/face1.jpg" alt="profile" />
-                    <span class="login-status online"></span>
-                    <!--change to offline or busy as needed-->
-                </div>
-                <div class="nav-profile-text d-flex flex-column">
-                    <span class="font-weight-bold mb-2">David Grey. H</span>
-                    <span class="text-secondary text-small">Project Manager</span>
-                </div>
-                <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('dashboard') }}">
-                <span class="menu-title">Dashboard</span>
-                <i class="mdi mdi-home menu-icon"></i>
-            </a>
-        </li>
-        {{-- <li class="nav-item">
-            <a class="nav-link" href="{{ route('driver.index') }}">
-                <span class="menu-title">Driver</span>
-                <i class="mdi mdi-truck-cargo-container menu-icon"></i>
-            </a>
-        </li> --}}
-        <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                aria-controls="ui-basic">
-                <span class="menu-title">Driver</span>
-                <i class="menu-arrow"></i>
-                <i class="mdi mdi-truck-cargo-container menu-icon"></i>
-            </a>
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand">
+        <!--begin::Brand Link-->
+        <a href="./index.html" class="brand-link">
+            <!--begin::Brand Image-->
+            <img src="./assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow" />
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">AdminLTE 4</span>
+            <!--end::Brand Text-->
+        </a>
+        <!--end::Brand Link-->
+    </div>
+    <!--end::Sidebar Brand-->
+    <!--begin::Sidebar Wrapper-->
+    <div class="sidebar-wrapper">
+        <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="navigation"
+                aria-label="Main navigation" data-accordion="false" id="navigation">
+                <li class="nav-item">
+                    <a href="./docs/introduction.html" class="nav-link">
+                        <i class="nav-icon bi bi-speedometer"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-box-seam-fill"></i>
+                        <p>
+                            Widgets
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="./widgets/small-box.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Small Box</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./widgets/info-box.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>info Box</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./widgets/cards.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Cards</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
 
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('driver.index') }}">Driver List</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('driver.fleet-overview') }}">Fleet Overview</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-    </ul>
-</nav>
-<!-- partial -->
+                <li class="nav-header">EXAMPLES</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-box-arrow-in-right"></i>
+                        <p>
+                            Auth
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon bi bi-box-arrow-in-right"></i>
+                                <p>
+                                    Version 1
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="./examples/login.html" class="nav-link">
+                                        <i class="nav-icon bi bi-circle"></i>
+                                        <p>Login</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="./examples/register.html" class="nav-link">
+                                        <i class="nav-icon bi bi-circle"></i>
+                                        <p>Register</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon bi bi-box-arrow-in-right"></i>
+                                <p>
+                                    Version 2
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="./examples/login-v2.html" class="nav-link">
+                                        <i class="nav-icon bi bi-circle"></i>
+                                        <p>Login</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="./examples/register-v2.html" class="nav-link">
+                                        <i class="nav-icon bi bi-circle"></i>
+                                        <p>Register</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./examples/lockscreen.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Lockscreen</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-header">DOCUMENTS</li>
+                <li class="nav-item">
+                    <a href="./docs/introduction.html" class="nav-link">
+                        <i class="nav-icon bi bi-download"></i>
+                        <p>Installation</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./docs/layout.html" class="nav-link">
+                        <i class="nav-icon bi bi-grip-horizontal"></i>
+                        <p>Layout</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./docs/color-mode.html" class="nav-link">
+                        <i class="nav-icon bi bi-star-half"></i>
+                        <p>Color Mode</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-ui-checks-grid"></i>
+                        <p>
+                            Components
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="./docs/components/main-header.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Main Header</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./docs/components/main-sidebar.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Main Sidebar</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-filetype-js"></i>
+                        <p>
+                            Javascript
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="./docs/javascript/treeview.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Treeview</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="./docs/browser-support.html" class="nav-link">
+                        <i class="nav-icon bi bi-browser-edge"></i>
+                        <p>Browser Support</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./docs/how-to-contribute.html" class="nav-link">
+                        <i class="nav-icon bi bi-hand-thumbs-up-fill"></i>
+                        <p>How To Contribute</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./docs/faq.html" class="nav-link">
+                        <i class="nav-icon bi bi-question-circle-fill"></i>
+                        <p>FAQ</p>
+                    </a>
+                </li>
+            </ul>
+                <!--end::Sidebar Menu-->
+        </nav>
+    </div>
+    <!--end::Sidebar Wrapper-->
+</aside>
